0331c4891b447542e50ee65255b108e89806d13d
Sagar Patel <sagarmp@cs.unc.edu>
2021-02-17 14:13:37 -0500

    first attempt

---
+++ README.md
@@ -0,0 +1,6 @@
+# COMP 431 - HW 3
+
+To get the starter code, run the following script:
+```shell
+./get-starter-code.sh
+```
---
+++ get-starter-code.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# set up error handling
+set -eux
+trap "_handle_error" 1 2 3 15
+_handle_error()
+{
+	echo "ERROR: Please send the output of this script to course staff."
+	exit 1
+}
+
+# collect variables before nuking this repo
+# use HTTPS URL in REAL_STARTER_CODE incase student hasn't setup SSH keys
+REAL_STARTER_CODE="https://github.com/comp431/real-hw3.git"
+STUDENT_REMOTE="$(git remote get-url origin)"
+
+# delete all files. More importantly, delete the existing git repo.
+rm -rf * .*
+
+# create new repo based off of real starter code
+git clone "$REAL_STARTER_CODE" .
+git remote remove origin
+git remote set-url origin "$STUDENT_REMOTE"
+git push --set-upstream origin master --force
+
+# add real starter code remote
+# if there are updates to the starter code, students can simply
+# `git pull starter master` to get the latest version without clobbering their
+# own work. They might need to resolve merge conflicts with future invocations.
+git remote add starter "$REAL_STARTER_CODE"
+git pull starter master
+
+echo "All done! Your starter code repo has been set up. Happy Hacking!"
